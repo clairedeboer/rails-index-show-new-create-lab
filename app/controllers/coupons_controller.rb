d5d31952058987623bbdd08010e140d6211e5528
clairedeboer <claire.deboer@gmail.com>
2020-12-10 21:34:38 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,23 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+  
+  def create
+    @coupon = Coupon.create (
+      {
+        coupon_code: params[:coupon_code], 
+        store: params[:store]
+      }
+    )
+    redirect_to coupons_path(@coupon)
+  end
+end
